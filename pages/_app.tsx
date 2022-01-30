54baf8ca7040f23de8285cc4af758ebd67f990fd
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-01-30 23:41:44 +0700

    Install @fontsource/poppins

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/700.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
