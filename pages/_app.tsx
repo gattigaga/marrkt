3c569e1735c48af690888c937c1beaaa1f60eed5
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-01 23:57:27 +0700

    Add poppins font bold support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
