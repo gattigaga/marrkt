8e2394a7a38ce993316bfeafab94f757aa28ab23
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-01 18:06:24 +0700

    Fix Cart component

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { gsap } from "gsap";
+
+import CartItem from "./CartItem";
+import Button from "./Button";
+import { numberToCurrency } from "../helpers/formatter";
 
 type CartProps = {
   isOpen?: boolean;
@@ -9,6 +13,67 @@ type CartProps = {
 const Cart: React.FC<CartProps> = ({ isOpen, onClickBackdrop }) => {
   const refBackdrop = useRef();
   const refCart = useRef();
+
+  const items = useMemo(
+    () => [
+      {
+        id: 1,
+        name: "LT 01 Alloy",
+        price: 2456,
+        quantity: 2,
+        image: "https://via.placeholder.com/128x128",
+      },
+      {
+        id: 2,
+        name: "LT 01 Alloy",
+        price: 2456,
+        quantity: 2,
+        image: "https://via.placeholder.com/128x128",
+      },
+      {
+        id: 3,
+        name: "LT 01 Alloy",
+        price: 2456,
+        quantity: 2,
+        image: "https://via.placeholder.com/128x128",
+      },
+      {
+        id: 4,
+        name: "LT 01 Alloy",
+        price: 2456,
+        quantity: 2,
+        image: "https://via.placeholder.com/128x128",
+      },
+      {
+        id: 5,
+        name: "LT 01 Alloy",
+        price: 2456,
+        quantity: 2,
+        image: "https://via.placeholder.com/128x128",
+      },
+      {
+        id: 6,
+        name: "LT 01 Alloy",
+        price: 2456,
+        quantity: 2,
+        image: "https://via.placeholder.com/128x128",
+      },
+      {
+        id: 7,
+        name: "LT 01 Alloy",
+        price: 2456,
+        quantity: 2,
+        image: "https://via.placeholder.com/128x128",
+      },
+    ],
+    []
+  );
+
+  const subtotal = useMemo(() => {
+    return items.reduce((acc, item) => {
+      return acc + item.price * item.quantity;
+    }, 0);
+  }, [items]);
 
   useEffect(() => {
     if (refBackdrop.current && refCart.current) {
@@ -23,7 +88,7 @@ const Cart: React.FC<CartProps> = ({ isOpen, onClickBackdrop }) => {
             duration: 0.2,
           })
           .to(refCart.current, {
-            height: "100%",
+            height: "auto",
             duration: 0.3,
           });
       } else {
@@ -52,10 +117,35 @@ const Cart: React.FC<CartProps> = ({ isOpen, onClickBackdrop }) => {
     >
       <div
         ref={refCart}
-        style={{ width: 400, height: "100%" }}
-        className="bg-white self-end"
+        style={{ width: 400 }}
+        className="bg-white self-end max-h-full flex flex-col px-6 pt-24 pb-6"
         onClick={(event) => event.stopPropagation()}
-      ></div>
+      >
+        <div className="max-h-full overflow-y-scroll">
+          {items.map((item, index) => {
+            const isLast = index === items.length - 1;
+
+            return (
+              <div key={item.id}>
+                <CartItem
+                  name={item.name}
+                  quantity={item.quantity}
+                  price={item.price}
+                  image="https://via.placeholder.com/128x128"
+                />
+                {!isLast && <div className="w-full border-t border-gray-200" />}
+              </div>
+            );
+          })}
+        </div>
+        <div className="flex items-center justify-between py-4 border-t border-b border-gray-200">
+          <p className="text-xs text-black">Subtotal</p>
+          <p className="text-xs text-black">{numberToCurrency(subtotal)}</p>
+        </div>
+        <div className="flex flex-col items-end mt-4">
+          <Button label="Checkout" />
+        </div>
+      </div>
     </div>
   );
 };
