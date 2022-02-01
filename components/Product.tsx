47b5aa5da68915baa878d0a61b35ea396641c207
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-01 23:54:16 +0700

    Fix url in Product component

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+
 import { numberToCurrency } from "../helpers/formatter";
 
 type ProductProps = {
@@ -12,21 +14,23 @@ const Product: React.FC<ProductProps> = ({ image, name, price, url }) => {
   const formattedPrice = numberToCurrency(price);
 
   return (
-    <a href={url}>
-      <figure>
-        <img
-          className="w-full aspect-square mb-2 sm:mb-3"
-          src={image}
-          alt={name}
-        />
-        <figcaption>
-          <p className="text-xs truncate text-ellipsis overflow-hidden font-medium sm:mb-1">
-            {name}
-          </p>
-          <p className="text-xs text-gray-500">{formattedPrice}</p>
-        </figcaption>
-      </figure>
-    </a>
+    <Link href={url}>
+      <a>
+        <figure>
+          <img
+            className="w-full aspect-square mb-2 sm:mb-3"
+            src={image}
+            alt={name}
+          />
+          <figcaption>
+            <p className="text-xs truncate text-ellipsis overflow-hidden font-medium sm:mb-1">
+              {name}
+            </p>
+            <p className="text-xs text-gray-500">{formattedPrice}</p>
+          </figcaption>
+        </figure>
+      </a>
+    </Link>
   );
 };
 
