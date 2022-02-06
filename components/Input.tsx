16c8970724723319094d05237446bf45d8f7792e
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-06 20:35:48 +0700

    Create Input component

---
+++ components/Input.tsx
@@ -0,0 +1,48 @@
+import React, { ChangeEvent, FocusEvent } from "react";
+
+type InputProps = {
+  label: string;
+  name: string;
+  id: string;
+  value: string;
+  type?: string;
+  disabled?: boolean;
+  hasError?: boolean;
+  errorText?: string;
+  onChange?: (event: ChangeEvent<any>) => void;
+  onBlur?: (event: FocusEvent<any, Element>) => void;
+};
+
+const Input: React.FC<InputProps> = ({
+  label,
+  name,
+  id,
+  value,
+  type = "text",
+  disabled,
+  hasError,
+  errorText,
+  onChange,
+  onBlur,
+}) => {
+  return (
+    <div>
+      <label className="text-xs text-black mb-2 block" htmlFor={id}>
+        {label}
+      </label>
+      <input
+        className="border border-gray-200 px-4 py-3 text-xs w-96"
+        id={id}
+        type={type}
+        name={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+        disabled={disabled}
+      />
+      {hasError && <p className="text-xs text-red-600 mt-2">{errorText}</p>}
+    </div>
+  );
+};
+
+export default Input;
