178b3d8d92d87ca15efec5a3f960b7e3fc686e16
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-07 22:43:33 +0700

    Create home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import Button from "../components/Button";
 import Menu from "../components/Menu";
 
 const HomePage: NextPage = () => {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -10,7 +14,16 @@ const HomePage: NextPage = () => {
       </Head>
 
       <Menu />
-      <main className="px-4 pt-28 pb-24 md:px-8"></main>
+      <main className="min-h-screen flex">
+        <div className="flex-1 flex flex-col justify-center pl-24">
+          <h1 className="text-8xl font-bold mb-2">marrkt.</h1>
+          <p className="text-sm mb-10">
+            Fashion that fit your style and up to date.
+          </p>
+          <Button label="Shop now" onClick={() => router.push("/products")} />
+        </div>
+        <img className="w-2/4 h-screen object-cover" src="/images/person.jpg" />
+      </main>
     </div>
   );
 };
