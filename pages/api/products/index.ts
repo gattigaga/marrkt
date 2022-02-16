21a9a4e6520f9f9896c204d070b6aae2734806cc
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-16 18:48:50 +0700

    Fix "GET /products" api

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { supabase } from "../../helpers/supabase";
+import { supabase } from "../../../helpers/supabase";
 
 type Product = {
   id: number;
