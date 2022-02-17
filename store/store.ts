d5895048609a479d99e94e2cea210e7cfa1c77d7
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-18 02:13:20 +0700

    Make store persistent

--- store/store.ts
+++ store/store.ts
@@ -1,4 +1,5 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
 type Product = {
   id: number;
@@ -22,73 +23,80 @@ type State = {
   decreaseItemQty: (itemId: string) => void;
 };
 
-export const useStore = create<State>((set) => ({
-  cartItems: [],
-  addToCart: (item: CartItem) => {
-    set((state) => {
-      const isItemAlreadyExists = state.cartItems.find((cartItem) => {
-        return cartItem.product.id === item.product.id;
-      });
+export const useStore = create(
+  persist<State>(
+    (set) => ({
+      cartItems: [],
+      addToCart: (item: CartItem) => {
+        set((state) => {
+          const isItemAlreadyExists = state.cartItems.find((cartItem) => {
+            return cartItem.product.id === item.product.id;
+          });
 
-      if (isItemAlreadyExists) {
-        return {
-          cartItems: state.cartItems.map((cartItem) => {
-            if (cartItem.product.id === item.product.id) {
-              return {
-                ...cartItem,
-                quantity: cartItem.quantity + 1,
-              };
-            }
+          if (isItemAlreadyExists) {
+            return {
+              cartItems: state.cartItems.map((cartItem) => {
+                if (cartItem.product.id === item.product.id) {
+                  return {
+                    ...cartItem,
+                    quantity: cartItem.quantity + 1,
+                  };
+                }
 
-            return cartItem;
-          }),
-        };
-      }
-
-      return {
-        cartItems: [...state.cartItems, item],
-      };
-    });
-  },
-  removeFromCart: (itemId: string) => {
-    set((state) => {
-      return {
-        cartItems: state.cartItems.filter((item) => item.id !== itemId),
-      };
-    });
-  },
-  increaseItemQty: (itemId: string) => {
-    set((state) => {
-      return {
-        cartItems: state.cartItems.map((item) => {
-          if (item.id === itemId) {
-            return { ...item, quantity: item.quantity + 1 };
+                return cartItem;
+              }),
+            };
           }
 
-          return item;
-        }),
-      };
-    });
-  },
-  decreaseItemQty: (itemId: string) => {
-    set((state) => {
-      const item = state.cartItems.find((item) => item.id === itemId);
+          return {
+            cartItems: [...state.cartItems, item],
+          };
+        });
+      },
+      removeFromCart: (itemId: string) => {
+        set((state) => {
+          return {
+            cartItems: state.cartItems.filter((item) => item.id !== itemId),
+          };
+        });
+      },
+      increaseItemQty: (itemId: string) => {
+        set((state) => {
+          return {
+            cartItems: state.cartItems.map((item) => {
+              if (item.id === itemId) {
+                return { ...item, quantity: item.quantity + 1 };
+              }
 
-      if (item?.quantity === 1) {
-        return {
-          cartItems: state.cartItems.filter((item) => item.id === itemId),
-        };
-      }
+              return item;
+            }),
+          };
+        });
+      },
+      decreaseItemQty: (itemId: string) => {
+        set((state) => {
+          const item = state.cartItems.find((item) => item.id === itemId);
 
-      return {
-        cartItems: state.cartItems.map((item) => {
-          if (item.id === itemId && item.quantity > 0) {
-            return { ...item, quantity: item.quantity - 1 };
+          if (item?.quantity === 1) {
+            return {
+              cartItems: state.cartItems.filter((item) => item.id === itemId),
+            };
           }
 
-          return item;
-        }),
-      };
-    });
-  },
-}));
+          return {
+            cartItems: state.cartItems.map((item) => {
+              if (item.id === itemId && item.quantity > 0) {
+                return { ...item, quantity: item.quantity - 1 };
+              }
+
+              return item;
+            }),
+          };
+        });
+      },
+    }),
+    {
+      name: "general-storage",
+    }
+  )
+);
