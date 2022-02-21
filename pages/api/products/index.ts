05deac905b498f408cc1f681787d00d518a2c42b
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-22 00:30:47 +0700

    Fix "GET /products" api

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,6 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "../../../helpers/supabase";
 
+type Category = {
+  id: number;
+  name: string;
+  slug: string;
+};
+
 type Product = {
   id: number;
   name: string;
@@ -9,6 +15,7 @@ type Product = {
   description: string;
   product_category_id: number;
   thumbnail: string;
+  category: Category;
 };
 
 type Data = {
@@ -26,84 +33,99 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     return;
   }
 
-  const {
-    keyword,
-    categories,
-    min_price,
-    max_price,
-    page = "1",
-  } = req.query as {
-    [key: string]: string;
-  };
+  try {
+    const {
+      keyword,
+      categories,
+      min_price,
+      max_price,
+      page = "1",
+    } = req.query as {
+      [key: string]: string;
+    };
 
-  const totalItemsPerPage = 12;
+    const totalItemsPerPage = 12;
 
-  const { data: products } = await (() => {
-    const query = supabase
-      .from("products")
-      .select("*, product_categories!inner(*)")
-      .order("name");
+    const { data: products, error: productsError } = await (() => {
+      const query = supabase
+        .from("products")
+        .select("*, category:product_categories!inner(*)")
+        .order("name");
 
-    if (keyword) {
-      query.ilike("name", `%${keyword}%`);
+      if (keyword) {
+        query.ilike("name", `%${keyword}%`);
+      }
+
+      if (categories) {
+        query.in("category.slug", categories.split(","));
+      }
+
+      if (min_price) {
+        query.gte("price", min_price);
+      }
+
+      if (max_price) {
+        query.lte("price", max_price);
+      }
+
+      const min = totalItemsPerPage * (Number(page) - 1);
+      const max = totalItemsPerPage * Number(page) - 1;
+
+      query.range(min, max);
+
+      return query;
+    })();
+
+    if (productsError) {
+      throw productsError;
     }
 
-    if (categories) {
-      query.in("product_categories.slug", categories.split(","));
+    const { data: totalPages, error: totalPagesError } = await (async () => {
+      const query = supabase
+        .from("products")
+        .select("*, category:product_categories!inner(*)", {
+          count: "exact",
+          head: true,
+        });
+
+      if (keyword) {
+        query.ilike("name", `%${keyword}%`);
+      }
+
+      if (categories) {
+        query.in("category.slug", categories.split(","));
+      }
+
+      if (min_price) {
+        query.gte("price", min_price);
+      }
+
+      if (max_price) {
+        query.lte("price", max_price);
+      }
+
+      const { count, error } = await query;
+
+      return {
+        data: Math.ceil(Number(count) / totalItemsPerPage) || 1,
+        error,
+      };
+    })();
+
+    if (totalPagesError) {
+      throw totalPagesError;
     }
 
-    if (min_price) {
-      query.gte("price", min_price);
-    }
-
-    if (max_price) {
-      query.lte("price", max_price);
-    }
-
-    const min = totalItemsPerPage * (Number(page) - 1);
-    const max = totalItemsPerPage * Number(page) - 1;
-
-    query.range(min, max);
-
-    return query;
-  })();
-
-  const totalPages = await (async () => {
-    const query = supabase
-      .from("products")
-      .select("*, product_categories!inner(*)", {
-        count: "exact",
-        head: true,
-      });
-
-    if (keyword) {
-      query.ilike("name", `%${keyword}%`);
-    }
-
-    if (categories) {
-      query.in("product_categories.slug", categories.split(","));
-    }
-
-    if (min_price) {
-      query.gte("price", min_price);
-    }
-
-    if (max_price) {
-      query.lte("price", max_price);
-    }
-
-    const { count } = await query;
-
-    return Math.ceil(Number(count) / totalItemsPerPage);
-  })();
-
-  res.status(200).json({
-    data: products,
-    metadata: {
-      page: Number(page),
-      totalPages,
-    },
-  });
+    res.status(200).json({
+      data: products,
+      metadata: {
+        page: Number(page),
+        totalPages,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default handler;
