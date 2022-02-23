0b30012e58c8045ec24f83252d8fc07c7aa78762
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-24 00:17:30 +0700

    Make OrderItem component to use next/image

--- components/OrderItem.tsx
+++ components/OrderItem.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 import { numberToCurrency } from "../helpers/formatter";
 
@@ -24,12 +25,14 @@ const OrderItem: React.FC<OrderItemProps> = ({
     <Link href={url}>
       <a>
         <div className="flex border-b border-gray-200 py-4">
-          <img
-            className="w-16 h-16 mr-6 object-cover"
+          <Image
+            className="w-16 h-16 object-cover"
             src={thumbnail}
             alt={code}
+            width={64}
+            height={64}
           />
-          <div className="flex-1">
+          <div className="flex-1 ml-6">
             <p className="text-xs text-black mb-2">{code}</p>
             <p className="text-xs text-gray-500">Total items: {totalItems}</p>
             <p className="text-xs text-gray-500">
