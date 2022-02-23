02f1af3490f62c5689c8367b8c048cc3278d98d0
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-24 00:22:35 +0700

    Make home page to use next/image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { useRouter } from "next/router";
+
 import Button from "../components/Button";
 import Menu from "../components/Menu";
 
@@ -15,7 +17,15 @@ const HomePage: NextPage = () => {
 
       <Menu />
       <main className="min-h-screen flex">
-        <img className="w-2/4 h-screen object-cover" src="/images/person.jpg" />
+        <div className="w-2/4 h-screen overflow-hidden">
+          <Image
+            className="object-cover"
+            src="/images/person.jpg"
+            alt="Person"
+            width={1366 / 2}
+            height={768}
+          />
+        </div>
         <div className="flex-1 flex flex-col justify-center items-center">
           <h1 className="text-7xl text-center font-bold mb-2">marrkt.</h1>
           <p className="text-xs text-center mb-10">
