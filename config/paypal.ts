dd110396629906d201235eed556064752085ba36
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-23 21:25:16 +0700

    Fix paypal config

--- config/paypal.ts
+++ config/paypal.ts
@@ -1,5 +1,5 @@
 const config = {
-  "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+  "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID as string,
   intent: "capture",
   currency: "USD",
 };
