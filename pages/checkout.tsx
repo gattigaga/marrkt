3844a79dcabc531ca48a781731583b4caec3dcb8
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-02-24 22:20:07 +0700

    Fix checkout page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -84,161 +84,167 @@ const CheckoutPage: NextPage = () => {
       <Menu ref={refMenu} />
       <main className="min-h-screen flex flex-col items-center">
         <div className="w-1/3 pt-28 pb-24">
-          <h1 className="text-md font-medium text-black mt-4 mb-8">Checkout</h1>
-          <p className="text-xs text-gray-500 mb-1">
-            Invoice:&nbsp;&nbsp;&nbsp;
-            <span className="text-black font-medium">INV/{invoiceCode}</span>
-          </p>
-          <p className="text-xs text-gray-500 mb-1">
-            Payment Method:&nbsp;&nbsp;&nbsp;
-            <span className="text-black font-medium">Paypal</span>
-          </p>
-          <table className="w-full mt-4">
-            <thead className="border-b-2 border-black">
-              <tr>
-                <th className="py-4">
-                  <p className="text-xs font-medium text-black text-left">
-                    Product
-                  </p>
-                </th>
-                <th className="py-4">
-                  <p className="text-xs font-medium text-black">QTY</p>
-                </th>
-                <th className="py-4">
-                  <p className="text-xs font-medium text-black text-right">
-                    Subtotal
-                  </p>
-                </th>
-              </tr>
-            </thead>
-            {!!cartItems.length && (
-              <tbody>
-                {cartItems.map((item) => {
-                  const { publicURL: thumbnailURL } = supabase.storage
-                    .from("general")
-                    .getPublicUrl(`products/${item.product.thumbnail}`);
-
-                  return (
-                    <tr key={item.id} className="border-b border-gray-200">
-                      <td className="py-4">
-                        <div className="flex">
-                          <Image
-                            className="w-16 h-16 object-cover"
-                            src={thumbnailURL as string}
-                            alt={item.product.name}
-                            width={64}
-                            height={64}
-                          />
-                          <div className="w-40 ml-6 mr-auto py-2">
-                            <p className="text-black text-xs font-medium truncate text-ellipsis overflow-hidden mb-1">
-                              {item.product.name}
-                            </p>
-                            <p className="text-gray-500 text-xs">
-                              {numberToCurrency(item.product.price)}
-                            </p>
-                          </div>
-                        </div>
-                      </td>
-                      <td>
-                        <p className="text-center text-xs text-black">
-                          {item.quantity}
-                        </p>
-                      </td>
-                      <td>
-                        <p className="text-xs text-black text-right">
-                          {numberToCurrency(item.product.price * item.quantity)}
-                        </p>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            )}
-            {!cartItems.length && (
-              <tbody>
+          <div className="p-8  border">
+            <h1 className="text-md font-medium text-black mt-4 mb-8">
+              Checkout
+            </h1>
+            <p className="text-xs text-gray-500 mb-1">
+              Invoice:&nbsp;&nbsp;&nbsp;
+              <span className="text-black font-medium">INV/{invoiceCode}</span>
+            </p>
+            <p className="text-xs text-gray-500 mb-1">
+              Payment Method:&nbsp;&nbsp;&nbsp;
+              <span className="text-black font-medium">Paypal</span>
+            </p>
+            <table className="w-full mt-4">
+              <thead className="border-b-2 border-black">
                 <tr>
-                  <td colSpan={4}>
-                    <p className="text-center text-xs mt-6">
-                      There&lsquo;s no items in the cart.
+                  <th className="py-4">
+                    <p className="text-xs font-medium text-black text-left">
+                      Product
                     </p>
-                  </td>
+                  </th>
+                  <th className="py-4">
+                    <p className="text-xs font-medium text-black">QTY</p>
+                  </th>
+                  <th className="py-4">
+                    <p className="text-xs font-medium text-black text-right">
+                      Subtotal
+                    </p>
+                  </th>
                 </tr>
-              </tbody>
-            )}
-          </table>
-          {!!cartItems.length && (
-            <>
-              <div className="w-full flex justify-between py-4 mb-8">
-                <p className="text-xs font-medium text-black">Subtotal</p>
-                <p className="text-lg text-black font-bold">
-                  {numberToCurrency(subtotal)}
-                </p>
-              </div>
-              <PayPalButtons
-                className="rounded-none w-full"
-                style={{
-                  color: "black",
-                  layout: "horizontal",
-                  height: 40,
-                  shape: "rect",
-                  label: "checkout",
-                  tagline: false,
-                }}
-                createOrder={(data, actions) => {
-                  return actions.order.create({
-                    purchase_units: [
-                      {
-                        invoice_id: invoiceCode,
-                        items: cartItems.map((item) => {
-                          return {
-                            name: item.product.name,
-                            quantity: `${item.quantity}`,
-                            category: "PHYSICAL_GOODS",
-                            unit_amount: {
-                              currency_code: "USD",
-                              value: `${item.product.price}`,
+              </thead>
+              {!!cartItems.length && (
+                <tbody>
+                  {cartItems.map((item) => {
+                    const { publicURL: thumbnailURL } = supabase.storage
+                      .from("general")
+                      .getPublicUrl(`products/${item.product.thumbnail}`);
+
+                    return (
+                      <tr key={item.id} className="border-b border-gray-200">
+                        <td className="py-4">
+                          <div className="flex">
+                            <Image
+                              className="w-16 h-16 object-cover"
+                              src={thumbnailURL as string}
+                              alt={item.product.name}
+                              width={64}
+                              height={64}
+                            />
+                            <div className="w-40 ml-6 mr-auto py-2">
+                              <p className="text-black text-xs font-medium truncate text-ellipsis overflow-hidden mb-1">
+                                {item.product.name}
+                              </p>
+                              <p className="text-gray-500 text-xs">
+                                {numberToCurrency(item.product.price)}
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <p className="text-center text-xs text-black">
+                            {item.quantity}
+                          </p>
+                        </td>
+                        <td>
+                          <p className="text-xs text-black text-right">
+                            {numberToCurrency(
+                              item.product.price * item.quantity
+                            )}
+                          </p>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              )}
+              {!cartItems.length && (
+                <tbody>
+                  <tr>
+                    <td colSpan={4}>
+                      <p className="text-center text-xs mt-6">
+                        There&lsquo;s no items in the cart.
+                      </p>
+                    </td>
+                  </tr>
+                </tbody>
+              )}
+            </table>
+            {!!cartItems.length && (
+              <>
+                <div className="w-full flex justify-between py-4 mb-8">
+                  <p className="text-xs font-medium text-black">Subtotal</p>
+                  <p className="text-lg text-black font-bold">
+                    {numberToCurrency(subtotal)}
+                  </p>
+                </div>
+                <PayPalButtons
+                  className="rounded-none w-full"
+                  style={{
+                    color: "black",
+                    layout: "horizontal",
+                    height: 40,
+                    shape: "rect",
+                    label: "checkout",
+                    tagline: false,
+                  }}
+                  createOrder={(data, actions) => {
+                    return actions.order.create({
+                      purchase_units: [
+                        {
+                          invoice_id: invoiceCode,
+                          items: cartItems.map((item) => {
+                            return {
+                              name: item.product.name,
+                              quantity: `${item.quantity}`,
+                              category: "PHYSICAL_GOODS",
+                              unit_amount: {
+                                currency_code: "USD",
+                                value: `${item.product.price}`,
+                              },
+                            };
+                          }),
+                          amount: {
+                            breakdown: {
+                              item_total: {
+                                currency_code: "USD",
+                                value: `${subtotal}`,
+                              },
                             },
-                          };
-                        }),
-                        amount: {
-                          breakdown: {
-                            item_total: {
-                              currency_code: "USD",
-                              value: `${subtotal}`,
-                            },
+                            value: `${subtotal}`,
                           },
-                          value: `${subtotal}`,
                         },
-                      },
-                    ],
-                  });
-                }}
-                onApprove={(data, actions) => {
-                  return actions.order.capture().then((details) => {
-                    const shipping = details.purchase_units[0].shipping;
-
-                    if (!shipping) {
-                      toast("Failed to get shipping data !");
-                      return;
-                    }
-
-                    checkout({
-                      person_name: shipping.name.full_name,
-                      address_1: shipping.address.address_line_1,
-                      address_2: shipping.address.address_line_2,
-                      admin_area_1: shipping.address.admin_area_1,
-                      admin_area_2: shipping.address.admin_area_2,
-                      postal_code: shipping.address.postal_code,
-                      country_code: shipping.address.country_code,
+                      ],
                     });
-                  });
-                }}
-                onError={() => {
-                  toast("Failed to checkout !");
-                }}
-              />
-            </>
-          )}
+                  }}
+                  onApprove={(data, actions) => {
+                    return actions.order.capture().then((details) => {
+                      const shipping = details.purchase_units[0].shipping;
+
+                      if (!shipping) {
+                        toast("Failed to get shipping data !");
+                        return;
+                      }
+
+                      checkout({
+                        person_name: shipping.name.full_name,
+                        address_1: shipping.address.address_line_1,
+                        address_2: shipping.address.address_line_2,
+                        admin_area_1: shipping.address.admin_area_1,
+                        admin_area_2: shipping.address.admin_area_2,
+                        postal_code: shipping.address.postal_code,
+                        country_code: shipping.address.country_code,
+                      });
+                    });
+                  }}
+                  onError={() => {
+                    toast("Failed to checkout !");
+                  }}
+                />
+              </>
+            )}
+          </div>
         </div>
       </main>
     </div>
