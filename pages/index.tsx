69bc206b14a6a15c58a5722d8265119e2d3bb909
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-04 01:22:06 +0700

    Fix image paths in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,10 @@ import { useRouter } from "next/router";
 import Button from "../components/Button";
 import Menu from "../components/Menu";
 import ShopValue from "../components/ShopValue";
+import imgFashion from "../public/images/fashion.jpg";
+import imgFashion1 from "../public/images/fashion-1.jpg";
+import imgClothes from "../public/images/clothes.jpg";
+import imgTextile from "../public/images/textile.jpg";
 
 type Props = {};
 
@@ -21,13 +25,7 @@ const HomePage: NextPage<Props> = ({}) => {
       <Menu />
       <main className="min-h-screen">
         <div className="pt-24 pb-48 px-6 md:px-8">
-          <Image
-            className="object-cover"
-            src="/images/fashion.jpg"
-            alt="Person"
-            width={1366}
-            height={480}
-          />
+          <Image className="object-cover" src={imgFashion1} alt="Person" />
           <p className="font-bold text-3xl text-black tracking-tighter md:text-8xl">
             #BEFASHIONABLE
           </p>
@@ -48,14 +46,14 @@ const HomePage: NextPage<Props> = ({}) => {
               index={0}
               title="WELL DESIGNED"
               description="All clothes are designed by experienced fashion designer. So this means the clothes makes you looks fashionable and standout in your meet up."
-              image="/images/clothes.jpg"
+              image={imgClothes}
             />
             <div className="h-20 md:h-32" />
             <ShopValue
               index={1}
               title="HIGH QUALITY MATERIALS"
               description="Built with high quality materials. So it will have good durability, even in the extreme weather."
-              image="/images/textile.jpg"
+              image={imgTextile}
               align="right"
             />
             <div className="h-20 md:h-32" />
@@ -63,7 +61,7 @@ const HomePage: NextPage<Props> = ({}) => {
               index={2}
               title="AFFORDABLE PRICE"
               description="Even if the clothes built with high quality materials and designed by experienced fashion designer, the price is still affordable."
-              image="/images/fashion.jpg"
+              image={imgFashion}
             />
           </div>
 
