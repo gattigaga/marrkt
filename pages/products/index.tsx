f57c7ac63355bce0a913c1f42df34d4b3bbd7d31
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-06 22:57:34 +0700

    Refactor products page

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -2,7 +2,6 @@ import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import queryString from "query-string";
-import { useEffect } from "react";
 
 import Filter from "../../components/Filter";
 import Layout from "../../components/Layout";
@@ -60,74 +59,62 @@ const ProductsPage: NextPage<Props> = ({
 
   const currentPage = Number(router.query.page as string) || 1;
 
-  useEffect(() => {
-    const isBrowser = typeof window !== "undefined";
-
-    if (isBrowser) {
-      const luxy = require("luxy.js");
-
-      luxy.init();
-    }
-  }, []);
-
   return (
-    <div id="luxy">
+    <Layout>
       <Head>
         <title>Products | Marrkt</title>
       </Head>
 
-      <Layout>
-        <main className="px-6 pt-28 pb-24 md:px-8">
-          <div className="flex flex-col md:flex-row">
-            <div className="flex-1 mb-16 md:mr-12">
-              {!!products.length && (
-                <>
-                  <div className="grid grid-cols-2 gap-4 mb-8 sm:gap-y-6 md:gap-y-8 lg:grid-cols-4">
-                    {products.map((product) => {
-                      const { publicURL: imageURL } = supabase.storage
-                        .from("general")
-                        .getPublicUrl(`products/${product.thumbnail}`);
+      <main className="px-6 pt-28 pb-24 md:px-8">
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-1 mb-16 md:mr-12">
+            {!!products.length && (
+              <>
+                <div className="grid grid-cols-2 gap-4 mb-8 sm:gap-y-6 md:gap-y-8 lg:grid-cols-4">
+                  {products.map((product) => {
+                    const { publicURL: imageURL } = supabase.storage
+                      .from("general")
+                      .getPublicUrl(`products/${product.thumbnail}`);
 
-                      return (
-                        <Product
-                          key={product.id}
-                          image={imageURL || ""}
-                          name={product.name}
-                          price={product.price}
-                          url={`/products/${product.slug}`}
-                        />
-                      );
-                    })}
-                  </div>
-                  <div className="flex justify-center">
-                    <Pagination
-                      currentPage={currentPage}
-                      totalPages={totalPages}
-                      onPageChange={(pageIndex) => {
-                        const query = queryString.stringify({
-                          ...router.query,
-                          page: pageIndex,
-                        });
+                    return (
+                      <Product
+                        key={product.id}
+                        image={imageURL || ""}
+                        name={product.name}
+                        price={product.price}
+                        url={`/products/${product.slug}`}
+                      />
+                    );
+                  })}
+                </div>
+                <div className="flex justify-center">
+                  <Pagination
+                    currentPage={currentPage}
+                    totalPages={totalPages}
+                    onPageChange={(pageIndex) => {
+                      const query = queryString.stringify({
+                        ...router.query,
+                        page: pageIndex,
+                      });
 
-                        router.push(`/products?${query}`);
-                      }}
-                    />
-                  </div>
-                </>
-              )}
-              {!products.length && (
-                <p className="text-xs text-black text-center mt-8">
-                  There&lsquo;s no products found.
-                </p>
-              )}
-            </div>
-            <div className="w-full border-t border-gray-200 pt-6 md:border-0 md:pt-0 md:w-64">
-              <Filter categories={categories} />
-            </div>
+                      router.push(`/products?${query}`);
+                    }}
+                  />
+                </div>
+              </>
+            )}
+            {!products.length && (
+              <p className="text-xs text-black text-center mt-8">
+                There&lsquo;s no products found.
+              </p>
+            )}
           </div>
-        </main>
-      </Layout>
-    </div>
+          <div className="w-full border-t border-gray-200 pt-6 md:border-0 md:pt-0 md:w-64">
+            <Filter categories={categories} />
+          </div>
+        </div>
+      </main>
+    </Layout>
   );
 };
 
