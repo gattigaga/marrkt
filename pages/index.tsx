baa4cc9e3cdab3c5ec21f9c4ce57dccf9673f472
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-06 16:17:54 +0700

    Fix home page responsiive

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import Button from "../components/Button";
 import Menu from "../components/Menu";
 import ShopValue from "../components/ShopValue";
+import Footer from "../components/Footer";
 import imgFashion from "../public/images/fashion.jpg";
 import imgFashion1 from "../public/images/fashion-1.jpg";
 import imgClothes from "../public/images/clothes.jpg";
@@ -24,20 +25,35 @@ const HomePage: NextPage<Props> = ({}) => {
 
       <Menu />
       <main className="min-h-screen">
-        <div className="pt-24 pb-48 px-6 md:px-8">
-          <Image className="object-cover" src={imgFashion1} alt="Person" />
-          <p className="font-bold text-3xl text-black tracking-tighter md:text-8xl">
-            #BEFASHIONABLE
-          </p>
-          <p className="text-md font-medium mb-24 md:mb-48 md:text-2xl">
-            EMBRACE YOUR APPEARANCE
-          </p>
-          <div className="mx-auto mb-24 md:w-2/3 md:mb-48">
-            <p className="text-center text-md md:text-2xl">
+        <div className="pb-40">
+          <div className="w-full h-screen relative mb-24 sm:mb-48">
+            <Image
+              className="object-cover"
+              src={imgFashion1}
+              alt="Person"
+              layout="fill"
+              objectFit="cover"
+            />
+            <div className="absolute w-full h-full top-0 left-0 flex flex-col items-center justify-center p-4 md:p-8">
+              <div className="w-full sm:w-3/4 lg:w-8/12 xl:w-1/2">
+                <p className="font-bold text-5xl text-white tracking-tighter text-center mb-4 sm:text-7xl md:text-8xl">
+                  HANDSOME AND BRAVE
+                </p>
+                <p className="text-sm font-medium text-white text-center sm:text-xl md:text-2xl">
+                  EMBRACE YOUR APPEARANCE
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Quote */}
+          <div className="flex flex-col items-center mx-auto mb-24 px-6 sm:w-2/3 sm:mb-48 lg:w-1/2">
+            <p className="text-center text-md mb-16 sm:text-lg md:mb-20 md:text-2xl">
               We only sell best quality of fashion clothes and has modern styles
               &mdash; make you confident when you meet up with your friends
               &mdash; and make you looks standout.
             </p>
+            <div className="w-16 border-t border-black" />
           </div>
 
           {/* Shop value list */}
@@ -65,14 +81,22 @@ const HomePage: NextPage<Props> = ({}) => {
             />
           </div>
 
-          <p className="text-center font-bold text-5xl tracking-tighter mb-8 md:mb-16 md:text-9xl">
-            WHAT ARE YOU WAITING FOR?
-          </p>
-          <div className="flex justify-center">
-            <Button label="Shop now" onClick={() => router.push("/products")} />
+          {/* Ask for shopping */}
+          <div className="mx-auto px-6 sm:w-2/3">
+            <p className="text-center font-bold text-5xl tracking-tighter mb-8 sm:text-6xl sm:mb-16 md:text-7xl lg:text-8xl xl:text-9xl xl:mb-20">
+              WHAT ARE YOU WAITING FOR?
+            </p>
+            <div className="flex justify-center">
+              <Button
+                label="Shop now"
+                onClick={() => router.push("/products")}
+              />
+            </div>
           </div>
         </div>
       </main>
+
+      <Footer />
     </div>
   );
 };
