07696d8b3c598e0fd8c3ab5b10d291234fdd90b5
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-06 22:48:47 +0700

    Initialize luxy for inertia scroll inside Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,9 @@
-import React, { forwardRef, useImperativeHandle, useRef } from "react";
+import React, {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+} from "react";
 
 import Footer from "./Footer";
 import Menu, { Exposed as MenuExposed } from "./Menu";
@@ -8,7 +13,7 @@ export type Exposed = {
 };
 
 type Props = {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
 };
 
 const Layout: React.ForwardRefRenderFunction<Exposed, Props> = (
@@ -28,11 +33,23 @@ const Layout: React.ForwardRefRenderFunction<Exposed, Props> = (
     runCartItemCountAnimation,
   }));
 
+  useEffect(() => {
+    const isBrowser = typeof window !== "undefined";
+
+    if (isBrowser) {
+      const luxy = require("luxy.js");
+
+      luxy.init();
+    }
+  }, []);
+
   return (
     <div>
       <Menu ref={refMenu} />
-      {children}
-      <Footer />
+      <div id="luxy">
+        {children}
+        <Footer />
+      </div>
     </div>
   );
 };
