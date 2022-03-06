acacf85b97a3dee4ecb34e77ccb6edb4c1e4d7c6
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-06 17:10:47 +0700

    Implement Layout component in profile page

--- pages/account/profile.tsx
+++ pages/account/profile.tsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 
 import AccountMenu from "../../components/AccountMenu";
 import Button from "../../components/Button";
-import Menu from "../../components/Menu";
+import Layout from "../../components/Layout";
 import Input from "../../components/Input";
 import { supabase } from "../../helpers/supabase";
 
@@ -53,114 +53,115 @@ const ProfilePage: NextPage<Props> = ({}) => {
         <title>Profile | Marrkt</title>
       </Head>
 
-      <Menu />
-      <main className="min-h-screen flex flex-col-reverse px-4 pt-28 pb-24 md:flex-row md:px-8">
-        <div className="flex-1 mt-16 md:mt-0 md:mr-16">
-          <AccountMenu />
-        </div>
-        <div className="md:w-3/4">
-          <h1 className="text-md font-medium text-black mb-8">Profile</h1>
-          <div>
-            <Formik
-              initialValues={{
-                firstName: user?.user_metadata.first_name || "",
-                lastName: user?.user_metadata.last_name || "",
-                email: user?.email || "",
-              }}
-              validationSchema={validationSchema}
-              onSubmit={async (values, { setSubmitting }) => {
-                try {
-                  setSubmitting(true);
-
-                  const { firstName, lastName, email } = values;
-
-                  const { error } = await supabase.auth.update({
-                    email,
-                    data: {
-                      first_name: firstName,
-                      last_name: lastName,
-                    },
-                  });
-
-                  if (error) throw error;
-
-                  toast("Profile successfully update.");
-                } catch (error: any) {
-                  console.log(error);
-                  toast(error.message || "Failed to update profile.");
-                } finally {
-                  setSubmitting(false);
-                }
-              }}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                isSubmitting,
-              }) => (
-                <form onSubmit={handleSubmit}>
-                  <div className="grid grid-cols-1 gap-6 mb-8 md:grid-cols-2">
-                    <div>
-                      <Input
-                        name="firstName"
-                        id="firstName"
-                        label="First Name*"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.firstName}
-                        disabled={isSubmitting}
-                        hasError={!!(errors.firstName && touched.firstName)}
-                        errorText={errors.firstName as string}
-                      />
-                    </div>
-                    <div>
-                      <Input
-                        name="lastName"
-                        id="lastName"
-                        label="Last Name*"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.lastName}
-                        disabled={isSubmitting}
-                        hasError={!!(errors.lastName && touched.lastName)}
-                        errorText={errors.lastName as string}
-                      />
-                    </div>
-                    <div className="md:col-span-2">
-                      <Input
-                        name="email"
-                        id="email"
-                        label="Email Address*"
-                        type="text"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                        disabled={isSubmitting}
-                        hasError={!!(errors.email && touched.email)}
-                        errorText={errors.email}
-                      />
-                    </div>
-                  </div>
-
-                  <div className="flex justify-between items-center">
-                    <Button
-                      type="submit"
-                      label="Update"
-                      isLoading={isSubmitting}
-                    />
-                  </div>
-                </form>
-              )}
-            </Formik>
+      <Layout>
+        <main className="min-h-screen flex flex-col-reverse px-4 pt-28 pb-24 md:flex-row md:px-8">
+          <div className="flex-1 mt-16 md:mt-0 md:mr-16">
+            <AccountMenu />
           </div>
-        </div>
-      </main>
+          <div className="md:w-3/4">
+            <h1 className="text-md font-medium text-black mb-8">Profile</h1>
+            <div>
+              <Formik
+                initialValues={{
+                  firstName: user?.user_metadata.first_name || "",
+                  lastName: user?.user_metadata.last_name || "",
+                  email: user?.email || "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={async (values, { setSubmitting }) => {
+                  try {
+                    setSubmitting(true);
+
+                    const { firstName, lastName, email } = values;
+
+                    const { error } = await supabase.auth.update({
+                      email,
+                      data: {
+                        first_name: firstName,
+                        last_name: lastName,
+                      },
+                    });
+
+                    if (error) throw error;
+
+                    toast("Profile successfully update.");
+                  } catch (error: any) {
+                    console.log(error);
+                    toast(error.message || "Failed to update profile.");
+                  } finally {
+                    setSubmitting(false);
+                  }
+                }}
+              >
+                {({
+                  values,
+                  errors,
+                  touched,
+                  handleChange,
+                  handleBlur,
+                  handleSubmit,
+                  isSubmitting,
+                }) => (
+                  <form onSubmit={handleSubmit}>
+                    <div className="grid grid-cols-1 gap-6 mb-8 md:grid-cols-2">
+                      <div>
+                        <Input
+                          name="firstName"
+                          id="firstName"
+                          label="First Name*"
+                          type="text"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.firstName}
+                          disabled={isSubmitting}
+                          hasError={!!(errors.firstName && touched.firstName)}
+                          errorText={errors.firstName as string}
+                        />
+                      </div>
+                      <div>
+                        <Input
+                          name="lastName"
+                          id="lastName"
+                          label="Last Name*"
+                          type="text"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.lastName}
+                          disabled={isSubmitting}
+                          hasError={!!(errors.lastName && touched.lastName)}
+                          errorText={errors.lastName as string}
+                        />
+                      </div>
+                      <div className="md:col-span-2">
+                        <Input
+                          name="email"
+                          id="email"
+                          label="Email Address*"
+                          type="text"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.email}
+                          disabled={isSubmitting}
+                          hasError={!!(errors.email && touched.email)}
+                          errorText={errors.email}
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex justify-between items-center">
+                      <Button
+                        type="submit"
+                        label="Update"
+                        isLoading={isSubmitting}
+                      />
+                    </div>
+                  </form>
+                )}
+              </Formik>
+            </div>
+          </div>
+        </main>
+      </Layout>
     </div>
   );
 };
