8f9df8add033cfb37206f71331663bf34ef8830a
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-06 22:59:30 +0700

    Refactor orders page

--- pages/account/orders/index.tsx
+++ pages/account/orders/index.tsx
@@ -1,6 +1,6 @@
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import queryString from "query-string";
 import BeatLoader from "react-spinners/BeatLoader";
@@ -64,94 +64,82 @@ const OrdersPage: NextPage<Props> = ({ orders, totalPages }) => {
 
   const currentPage = Number(router.query.page as string) || 1;
 
-  useEffect(() => {
-    const isBrowser = typeof window !== "undefined";
-
-    if (isBrowser) {
-      const luxy = require("luxy.js");
-
-      luxy.init();
-    }
-  }, []);
-
   return (
-    <div id="luxy">
+    <Layout>
       <Head>
         <title>Orders | Marrkt</title>
       </Head>
 
-      <Layout>
-        <main className="min-h-screen flex flex-col-reverse px-4 pt-28 pb-24 md:flex-row md:px-8">
-          {!isLoading && (
-            <>
-              <div className="flex-1 mt-16 md:mt-0 md:mr-16">
-                <AccountMenu
-                  onLogoutStart={() => setIsLoading(true)}
-                  onLogoutEnd={() => setIsLoading(false)}
-                />
-              </div>
-              <div className="md:w-3/4">
-                <h1 className="text-md font-medium text-black mb-8">Orders</h1>
-                {!!orders.length && (
-                  <>
-                    <div className="mb-8">
-                      {orders.map((order) => {
-                        const { publicURL: thumbnailURL } = supabase.storage
-                          .from("general")
-                          .getPublicUrl(
-                            `products/${order.items[0].product.thumbnail}`
-                          );
-
-                        return (
-                          <OrderItem
-                            key={order.id}
-                            code={order.invoice_code}
-                            thumbnail={thumbnailURL || ""}
-                            totalItems={order.items_count}
-                            amount={order.total}
-                            date={order.created_at}
-                            url={`/account/orders/${order.invoice_code}`}
-                          />
-                        );
-                      })}
-                    </div>
-                    <div className="flex justify-center">
-                      <Pagination
-                        currentPage={currentPage}
-                        totalPages={totalPages}
-                        onPageChange={(pageIndex) => {
-                          const query = queryString.stringify({
-                            ...router.query,
-                            page: pageIndex,
-                          });
-
-                          router.push(`/account/orders?${query}`);
-                        }}
-                      />
-                    </div>
-                  </>
-                )}
-                {!orders.length && (
-                  <div>
-                    <p className="text-xs text-black">
-                      There&lsquo;s no orders found.
-                    </p>
-                  </div>
-                )}
-              </div>
-            </>
-          )}
-          {isLoading && (
-            <div className="w-full mt-40 flex flex-col items-center">
-              <BeatLoader color="black" size={24} loading />
-              <p className="mt-6 text-md text-black text-center">
-                Signing out...
-              </p>
+      <main className="min-h-screen flex flex-col-reverse px-4 pt-28 pb-24 md:flex-row md:px-8">
+        {!isLoading && (
+          <>
+            <div className="flex-1 mt-16 md:mt-0 md:mr-16">
+              <AccountMenu
+                onLogoutStart={() => setIsLoading(true)}
+                onLogoutEnd={() => setIsLoading(false)}
+              />
             </div>
-          )}
-        </main>
-      </Layout>
-    </div>
+            <div className="md:w-3/4">
+              <h1 className="text-md font-medium text-black mb-8">Orders</h1>
+              {!!orders.length && (
+                <>
+                  <div className="mb-8">
+                    {orders.map((order) => {
+                      const { publicURL: thumbnailURL } = supabase.storage
+                        .from("general")
+                        .getPublicUrl(
+                          `products/${order.items[0].product.thumbnail}`
+                        );
+
+                      return (
+                        <OrderItem
+                          key={order.id}
+                          code={order.invoice_code}
+                          thumbnail={thumbnailURL || ""}
+                          totalItems={order.items_count}
+                          amount={order.total}
+                          date={order.created_at}
+                          url={`/account/orders/${order.invoice_code}`}
+                        />
+                      );
+                    })}
+                  </div>
+                  <div className="flex justify-center">
+                    <Pagination
+                      currentPage={currentPage}
+                      totalPages={totalPages}
+                      onPageChange={(pageIndex) => {
+                        const query = queryString.stringify({
+                          ...router.query,
+                          page: pageIndex,
+                        });
+
+                        router.push(`/account/orders?${query}`);
+                      }}
+                    />
+                  </div>
+                </>
+              )}
+              {!orders.length && (
+                <div>
+                  <p className="text-xs text-black">
+                    There&lsquo;s no orders found.
+                  </p>
+                </div>
+              )}
+            </div>
+          </>
+        )}
+        {isLoading && (
+          <div className="w-full mt-40 flex flex-col items-center">
+            <BeatLoader color="black" size={24} loading />
+            <p className="mt-6 text-md text-black text-center">
+              Signing out...
+            </p>
+          </div>
+        )}
+      </main>
+    </Layout>
   );
 };
 
