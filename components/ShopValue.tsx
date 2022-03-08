6ad4f1829cc3fdcea3f1e5ab7b3154bdd5ff02f9
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-09 02:15:10 +0700

    Implement animation in ShopValue component

--- components/ShopValue.tsx
+++ components/ShopValue.tsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 import Image from "next/image";
+import { gsap, Power2 } from "gsap";
+
+export type Exposed = {
+  runShowUpAnimation: () => void;
+};
 
 type Props = {
   index?: number;
@@ -7,34 +12,76 @@ type Props = {
   description: string;
   image: string | StaticImageData;
   align?: "left" | "right";
+  eventName?: string;
 };
 
-const ShopValue: React.FC<Props> = ({
-  index = 0,
-  title,
-  description,
-  image,
-  align = "left",
-}) => {
+const ShopValue: React.ForwardRefRenderFunction<Exposed, Props> = (
+  { index = 0, title, description, image, align = "left", eventName },
+  ref
+) => {
+  const refImage = useRef(null);
+  const refText = useRef(null);
+
   const containerDirection =
     align === "left" ? "sm:flex-row" : "sm:flex-row-reverse";
   const contentDirection = align === "left" ? "items-start" : "items-end";
   const textAlign = align === "left" ? "text-left" : "text-right";
 
+  const runShowUpAnimation = () => {
+    gsap.fromTo(
+      refImage.current,
+      {
+        opacity: 0,
+        scale: 2,
+      },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        ease: Power2.easeOut,
+      }
+    );
+
+    gsap.fromTo(
+      refText.current,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        duration: 1,
+        delay: 0.4,
+        ease: Power2.easeOut,
+      }
+    );
+  };
+
+  useImperativeHandle(ref, () => ({
+    runShowUpAnimation,
+  }));
+
   return (
-    <div className={`flex flex-col ${containerDirection}`}>
-      <div className="w-full aspect-square relative sm:w-80 md:w-96 lg:w-1/2">
-        <Image
-          className="object-cover"
-          src={image}
-          alt={title}
-          layout="fill"
-          objectFit="cover"
-        />
+    <div
+      className={`flex flex-col ${containerDirection}`}
+      data-scroll-call={eventName}
+      data-scroll-offset="40%"
+      data-scroll
+    >
+      <div
+        className="w-full aspect-square relative overflow-hidden sm:w-80 md:w-96 lg:w-1/2"
+        data-scroll-speed="1"
+        data-scroll
+      >
+        <div ref={refImage} className="w-full h-full opacity-0">
+          <Image src={image} alt={title} layout="fill" objectFit="cover" />
+        </div>
       </div>
       <div className="w-8 h-8 lg:w-12" />
       <div
-        className={`flex flex-1 flex-col ${contentDirection} px-6 sm:px-0 sm:pt-4 lg:pt-8 xl:pt-12`}
+        ref={refText}
+        className={`flex flex-1 flex-col ${contentDirection} px-6 opacity-0 sm:px-0 sm:pt-4 lg:pt-8 xl:pt-12`}
+        data-scroll-speed="3"
+        data-scroll
       >
         <p className={`font-medium text-xl text-black ${textAlign}`}>
           0{index + 1}
@@ -53,4 +100,4 @@ const ShopValue: React.FC<Props> = ({
   );
 };
 
-export default ShopValue;
+export default forwardRef(ShopValue);
