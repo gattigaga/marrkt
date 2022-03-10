7d5d63d74b4257676e09e482aef41c34cbf9f3af
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-03-11 01:50:19 +0700

    Fix scroll in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -12,11 +12,16 @@ const NotFoundPage: NextPage<Props> = ({}) => {
         <title>404 | Marrkt</title>
       </Head>
 
-      <main className="min-h-screen flex flex-col items-center justify-center">
-        <h1 className="font-bold text-black text-8xl mb-4">404</h1>
-        <p className="text-center text-black">
-          There&lsquo;s no content found here.
-        </p>
+      <main className="min-h-screen">
+        <div
+          className="w-full h-screen flex flex-col items-center justify-center"
+          data-scroll-section
+        >
+          <h1 className="font-bold text-black text-8xl mb-4">404</h1>
+          <p className="text-center text-black">
+            There&lsquo;s no content found here.
+          </p>
+        </div>
       </main>
     </Layout>
   );
