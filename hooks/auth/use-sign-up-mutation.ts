f4f57bdcaff8e74359b7986357d8a197fd107d03
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2022-05-12 22:35:18 +0700

    Fix "useSignUpMutation" hook

--- hooks/auth/use-sign-up-mutation.ts
+++ hooks/auth/use-sign-up-mutation.ts
@@ -4,8 +4,8 @@ import { useMutation } from "react-query";
 type Response = any;
 
 type Body = {
-  fullname: string;
-  username: string;
+  first_name: string;
+  last_name: string;
   email: string;
   password: string;
   confirm_password: string;
